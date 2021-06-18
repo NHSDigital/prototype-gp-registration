ae7df54a45ac564f8d3384645eddf5014a1af268
danjohnstonUX <daniel.johnston1@nhs.net>
2021-06-18 15:55:09 +0100

    added start of discovery prototype flow

--- docs/documentation_routes.js
+++ docs/documentation_routes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 // Documentation router
-router.get('/', function(req , res){ 
+router.get('/', function(req , res){
   res.render('index');
 });
 
