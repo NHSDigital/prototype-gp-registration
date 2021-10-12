4db061418c5078743e0eb76343c76cb5cfc507b1
Robincoxrjc <84479258+Robincoxrjc@users.noreply.github.com>
2021-10-12 11:50:23 +0100

    added dev mode
    added dev mode

--- app/assets/javascript/main.js
+++ app/assets/javascript/main.js
@@ -1,1 +1,15 @@
-// ES6 or Vanilla JavaScript
+// dev mode
+
+function toggleDevMode() {
+    const devModeButton = document.querySelector('#nhsuk-footer ul li a[href*="#"]');
+    devModeButton.addEventListener('click', function (event) {
+        event.preventDefault();
+        if (window.location.href.indexOf('devMode=') === -1) {
+            window.location.href = window.location.href + '?devMode=1';
+        } else {
+            window.location.href = '/clear-data';
+        }
+    }, false);
+}
+
+toggleDevMode();
