7bbe12ca43c96ed0df8b16e715bd5a0bbf5b1eab
Andy Farmer <andyfarmer@Andys-MacBook-Air.local>
2026-04-10 17:29:55 +0100

    mpr removals - add 8-day warning and warning date pages

--- app/views/mpr-removals/routes.js
+++ app/views/mpr-removals/routes.js
@@ -1,4 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+router.post('/main/8-day/warning-answer', function(req, res) {
+
+	const country = req.session.data['8dayWarning'];
+	if (country == "yes"){
+		res.redirect("/mpr-removals/main/8-day/warning-date")
+	} else {
+		res.redirect("/mpr-removals/main/8-day/warning-reasons")
+	}
+})
+
 module.exports = router;
